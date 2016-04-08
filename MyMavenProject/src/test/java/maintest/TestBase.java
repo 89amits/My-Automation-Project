06d0003de6c24c743193a2629da24ab2c5eead5b
AmitS <AmitS@LAPTOP-GN1D68B5>
2016-04-08 22:44:35 +0530

    Working project

--- MyMavenProject/src/test/java/maintest/TestBase.java
+++ MyMavenProject/src/test/java/maintest/TestBase.java
@@ -1,11 +1,16 @@
 package maintest;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestBase {
   @Test
-  public void f() {
+  public void f() throws InterruptedException {
   System.out.println("hello");
+  FirefoxDriver driver = new FirefoxDriver();
+  driver.get("https://google.com");
+  Thread.sleep(1000);
+  driver.close();
   }
   
 }
