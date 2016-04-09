577a6e27a10d154247d757acbe881581c1462d24
AmitS <AmitS@LAPTOP-GN1D68B5>
2016-04-09 13:05:15 +0530

    make use of testng

--- MyMavenProject/src/test/java/maintest/TestBase.java
+++ MyMavenProject/src/test/java/maintest/TestBase.java
@@ -1,16 +1,29 @@
 package maintest;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestBase {
   @Test
-  public void f() throws InterruptedException {
+  @Parameters({"browser_name"})
+  public void f(String browser_name) throws InterruptedException {
   System.out.println("hello");
+  if(browser_name.equalsIgnoreCase("Firefox")){
   FirefoxDriver driver = new FirefoxDriver();
   driver.get("https://google.com");
   Thread.sleep(1000);
-  driver.close();
+  }
+  else if(browser_name.equalsIgnoreCase("Chrome")){
+	  String Chrome_Path="F://selenium//Jars//chromedriver.exe";
+	  System.setProperty("webdriver.chrome.driver",Chrome_Path);
+	  ChromeDriver driver = new ChromeDriver();
+	 // DesiredCapabilities cap = new DesiredCapabilities();
+	  driver.get("https://google.com");
+	  
   }
   
+}
 }
