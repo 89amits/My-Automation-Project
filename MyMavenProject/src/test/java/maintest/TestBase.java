8d5608cdcb022bcb3941e936d332a0a4eb9f3dbe
AmitS <AmitS@LAPTOP-GN1D68B5>
2016-04-10 22:31:53 +0530

    Excel Read and Excel Write file checked in

--- MyMavenProject/src/test/java/maintest/TestBase.java
+++ MyMavenProject/src/test/java/maintest/TestBase.java
@@ -2,6 +2,7 @@ package maintest;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
@@ -12,19 +13,23 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import Utils.ExcelReader;
+import Utils.ExcelWrite;
+
 public class TestBase {
-	File f=new File("F://Git//MyMavenProject//Config.file");
-	FileInputStream fis =new FileInputStream(f);
-    Properties prop = new Properties();
-    prop.load(fis);
+	
   @Test
   @Parameters({"browser_name"})
-  public void f(String browser_name) throws InterruptedException {
+  public void f(String browser_name) throws InterruptedException, IOException {
+	File f=new File("F://Git//MyMavenProject//Config.file");
+		FileInputStream fis =new FileInputStream(f);
+	   Properties prop = new Properties();
+	   prop.load(fis);
 	  //String browser_name="Chrome";
   System.out.println("hello");
   if(browser_name.equalsIgnoreCase("Firefox")){
   FirefoxDriver driver = new FirefoxDriver();
-  driver.get("https://google.com");
+  driver.get(prop.getProperty("BaseUrl"));
   Thread.sleep(1000);
   }
   else if(browser_name.equalsIgnoreCase("Chrome")){
@@ -32,7 +37,7 @@ public class TestBase {
 	  System.setProperty("webdriver.chrome.driver",Chrome_Path);
 	  ChromeDriver driver = new ChromeDriver();
 	 // DesiredCapabilities cap = new DesiredCapabilities();
-	  driver.get("https://google.com");
+	  driver.get(prop.getProperty("BaseUrl"));
 	  
   }
   else {
@@ -40,9 +45,12 @@ public class TestBase {
 	  System.setProperty("webdriver.IE.driver",IE_Path);
 	  WebDriver driver = new InternetExplorerDriver();
 	 // DesiredCapabilities cap = new DesiredCapabilities();
-	  driver.get("https://google.com");
+	  driver.get(prop.getProperty("BaseUrl"));
 	  
   }
-  
+  String val = ExcelReader.ExcelRead(1, 1);
+  String Result ="Check";
+  ExcelWrite.ExcelWrite(Result, 0, 0);
+  System.out.println(val);
 }
 }
